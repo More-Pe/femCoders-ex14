d99a53c9a0366010ff76f0c974c702e31df2472d
More-Pe <peraltamorena.va@gmail.com>
2025-05-14 03:29:40 +0200

    feature: add Strawberry Fields solution

--- StrawberryFields.java
+++ StrawberryFields.java
@@ -5,7 +5,12 @@ public class StrawberryFields {
     //Al final está la letra completa
     
     public static void main(String[] args) {
-
+        System.out.println("------------------ \uD83C\uDFB6Strawberry Fields Forever \uD83C\uDFB6------------------");
+        playChorus();
+        for (int number = 1; number <=3; number++){
+            playVersus(number);
+            playChorus();
+        }
     }
 
     /**
@@ -16,7 +21,31 @@ public class StrawberryFields {
      * Inside the function:
      * 1. print versus according the position number
      */
-    
+    public static  void playVersus(int number){
+        switch (number){
+            case 1:
+                System.out.println("Living is easy with eyes closed\n" +
+                        "Misunderstanding all you see\n" +
+                        "It's getting hard to be someone, but it all works out\n" +
+                        "It doesn't matter much to me\n");
+                break;
+            case 2:
+                System.out.println("No one I think is in my tree\n" +
+                        "I mean, it must be high or low\n" +
+                        "That is, you can't, you know, tune in, but it's alright\n" +
+                        "That is, I think it's not too bad\n");
+                break;
+            case 3:
+                System.out.println("Always, no sometimes, think it's me\n" +
+                        "But you know, I know when it's a dream\n" +
+                        "I think I know, I mean a yes\n" +
+                        "But it's all wrong\n" +
+                        "That is, I think I disagree\n");
+                break;
+            default:
+                break;
+        }
+    }
 
     /**
      * Function name: playChorus
@@ -24,7 +53,13 @@ public class StrawberryFields {
      * Inside the function:
      * 1. print chorus 
      */
-    
+    public static void playChorus(){
+        System.out.println("Let me take you down\n" +
+                "'Cause I'm going to strawberry fields\n" +
+                "Nothing is real\n" +
+                "And nothing to get hung about\n" +
+                "Strawberry fields forever\n");
+    }
 
 }
 
